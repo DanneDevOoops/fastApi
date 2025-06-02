09e1ecf2411ffa17173c9cde4517d9e24b7d2e80
Daniel Andersson <d.andersson86@icloud.com>
2025-06-02 11:46:58 +0200

    add auth_model.py to contain validation models for pydantic on user signin

---
+++ src/db/models/v2_models/auth_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Auth model description here...
+"""
+
+from pydantic import BaseModel, field_serializer
+
+from db.serializers.v2_serializers.v2_model_serializers import model_serialize
+from src.db.models.v2_models.user_model import User
+
+
+class AuthTokenResponse(BaseModel):
+    """
+    Model for the authentication token response.
+    """
+    token: str
+    token_type: str
+    user: User
+    expires_in: int
+
+    @field_serializer('user')
+    def remove_sensitive_user_data(self, user: User) -> User:
+        """
+        Serialize the user object to a dictionary and remove sensitive data.
+
+        :param user: The User object to serialize.
+        :return: The User object with sensitive data removed.
+        :rtype: User
+        """
+        model_serialize(user).pop('password', None)
+        return user
+
+
+class UserPasswordSignin(BaseModel):
+    """
+    Model for user password sign-in.
+    """
+    email: str
+    username: str
+    password: str
